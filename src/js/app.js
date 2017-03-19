bc96a0d455dcbb116324f8c48ecfb04ef0368139
Karl Lin <linroex@coder.tw>
2017-03-19 20:55:28 +0800

    countdown

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,31 @@
 var timer_id = 0;
 
 $("#start_timer").click(function() {
-  timer_id = setInterval(function() {
-    console.log("1");
-  }, 1000);
+  countdown();
 })
+
+countdown = function() {
+  var current_time = $("#timer .insider").text().trim().split(":");
+  current_time = parseInt(current_time[0]) * 60 + parseInt(current_time[1]);
+
+  timer_id = setInterval(function () {
+    if(current_time > 0) {
+      current_time -= 1;
+      var time_text = padleft(Math.floor(current_time / 60), 2) + ":" + padleft(Math.floor(current_time % 60), 2);
+      $("#timer .insider").text(time_text);
+    }else{
+      clearInterval(timer_id);
+    }
+  }, 1000);
+}
+
+padleft = function(str, size) {
+  str = String(str);
+  if(str.length < size) {
+    for(var i = 0; i < size - str.length; i++) {
+      str = "0" + str;
+    }
+    return str;
+  }
+  return str;
+}

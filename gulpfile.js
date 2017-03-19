2f962c0a59c3305ab81a57b7249403941857ee5b
Karl Lin <linroex@coder.tw>
2017-03-19 18:38:33 +0800

    gulp init

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+gulp.task('build_css', function() {
+  return gulp.src('./src/css/*.css')
+    .pipe(sass())
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('build', function () {
+  gulp.start('build_css');
+  gulp.src('src/fonts/*').pipe(gulp.dest('dist/fonts/'));
+  gulp.src('src/js').pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/css/*.scss', ['build_css']);
+})
+
+gulp.task('default', ['build']);

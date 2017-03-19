1259acae2366672fcb4b418ba2b6c122f23eb9a5
Karl Lin <linroex@coder.tw>
2017-03-19 23:51:46 +0800

    add icons

--- load.js
+++ load.js
@@ -1,10 +1,12 @@
 const remote = require('electron').remote;
 const notifier = require('node-notifier');
+const path = require('path');
 
 function notify(msg) {
   notifier.notify({
     title: 'Pomodoro',
     message: msg,
+    icon: path.join(__dirname, 'tomato.icns'),
     sound: true,
     wait: true
   });
